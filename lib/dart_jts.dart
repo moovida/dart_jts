f3cec52e423b517727e1d382f39630ae4761a73a
Andrea Antonello <andrea.antonello@gmail.com>
2020-05-06 10:22:17 +0200

    quotes

--- lib/dart_jts.dart
+++ lib/dart_jts.dart
@@ -1,13 +1,13 @@
 /// Entry point for the dart_jts library.
 library dart_jts;
 
-import "dart:collection";
-import "dart:core" ;
-import "dart:typed_data" ;
+import 'dart:collection';
+import 'dart:core';
+import 'dart:typed_data';
 import 'package:intl/intl.dart';
-import "dart:math" as math;
+import 'dart:math' as math;
 import 'package:collection/collection.dart' show ListEquality;
-import "package:hex/hex.dart";
+import 'package:hex/hex.dart';
 
 part 'src/com/hydrologis/dart_jts/io/io.dart';
 part 'src/com/hydrologis/dart_jts/io/wkb.dart';
@@ -26,10 +26,10 @@ part 'src/com/hydrologis/dart_jts/operation/predicate.dart';
 part 'src/com/hydrologis/dart_jts/operation/buffer.dart';
 part 'src/com/hydrologis/dart_jts/operation/buffer_validate.dart';
 part 'src/com/hydrologis/dart_jts/noding/noding.dart';
-part "src/com/hydrologis/dart_jts/math/math.dart";
-part "src/com/hydrologis/dart_jts/geomgraph/geomgraph.dart";
-part "src/com/hydrologis/dart_jts/geomgraph/index.dart";
-part "src/com/hydrologis/dart_jts/util.dart";
+part 'src/com/hydrologis/dart_jts/math/math.dart';
+part 'src/com/hydrologis/dart_jts/geomgraph/geomgraph.dart';
+part 'src/com/hydrologis/dart_jts/geomgraph/index.dart';
+part 'src/com/hydrologis/dart_jts/util.dart';
 part 'src/com/hydrologis/dart_jts/geom/geometry.dart';
 part 'src/com/hydrologis/dart_jts/geom/point.dart';
 part 'src/com/hydrologis/dart_jts/geom/geometry_collection.dart';
@@ -38,11 +38,9 @@ part 'src/com/hydrologis/dart_jts/geom/linestring.dart';
 part 'src/com/hydrologis/dart_jts/geom/multilinestring.dart';
 part 'src/com/hydrologis/dart_jts/geom/polygon.dart';
 part 'src/com/hydrologis/dart_jts/geom/multipolygon.dart';
-part "src/com/hydrologis/dart_jts/util/geom_impl.dart";
-part "src/com/hydrologis/dart_jts/util/util.dart";
-part "src/com/hydrologis/dart_jts/index/index.dart";
-part "src/com/hydrologis/dart_jts/index/chain.dart";
-part "src/com/hydrologis/dart_jts/index/strtree.dart";
-part "src/com/hydrologis/dart_jts/util/avltree.dart";
-
-
+part 'src/com/hydrologis/dart_jts/util/geom_impl.dart';
+part 'src/com/hydrologis/dart_jts/util/util.dart';
+part 'src/com/hydrologis/dart_jts/index/index.dart';
+part 'src/com/hydrologis/dart_jts/index/chain.dart';
+part 'src/com/hydrologis/dart_jts/index/strtree.dart';
+part 'src/com/hydrologis/dart_jts/util/avltree.dart';
