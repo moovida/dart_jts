df3292670ab959b1cc5bd5c46144175b01f7539e
Mohammed Aljezawi <killerm250m@gmail.com>
2021-12-04 11:09:24 +0200

    added Geometry Densifier and GeometryTransformer classes, i do not have knowledge to write test cases sorry about that.

---
+++ lib/src/com/hydrologis/dart_jts/densify/densifier.dart
@@ -0,0 +1,189 @@
+import 'package:dart_jts/dart_jts.dart';
+
+
+/*
+ * Copyright (c) 2016 Vivid Solutions.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+/**
+ * Densifies a {@link Geometry} by inserting extra vertices along the line segments
+ * contained in the geometry.
+ * All segments in the created densified geometry will be <b>no longer</b>
+ * than the given distance tolerance
+ * (that is, all segments in the output will have length less than or equal to
+ * the distance tolerance).
+ * The coordinates created during densification respect the input geometry's
+ * {@link PrecisionModel}.
+ * <p>
+ * By default polygonal results are processed to ensure they are valid.
+ * This processing is costly, and it is very rare for results to be invalid.
+ * Validation processing can be disabled by calling the {@link #setValidate(bool)} method.
+ * <p>
+ * <b>Note:</b> At some future point this class will
+ * offer a variety of densification strategies.
+ *
+ * @author Martin Davis
+ */
+class Densifier {
+  /**
+   * Densifies a geometry using a given distance tolerance,
+   * and respecting the input geometry's {@link PrecisionModel}.
+   *
+   * @param geom the geometry to densify
+   * @param distanceTolerance the distance tolerance to densify
+   * @return the densified geometry
+   */
+  static Geometry densify(Geometry geom, double distanceTolerance) {
+    Densifier densifier = new Densifier(geom);
+    densifier.setDistanceTolerance(distanceTolerance);
+    return densifier.getResultGeometry();
+  }
+
+  /**
+   * Densifies a list of coordinates.
+   *
+   * @param pts the coordinate list
+   * @param distanceTolerance the densify tolerance
+   * @return the densified coordinate sequence
+   */
+  static List<Coordinate> densifyPoints(List<Coordinate> pts,
+      double distanceTolerance, PrecisionModel precModel) {
+    LineSegment seg = new LineSegment.empty();
+    CoordinateList coordList = new CoordinateList();
+    for (int i = 0; i < pts.length - 1; i++) {
+      seg.p0 = pts[i];
+      seg.p1 = pts[i + 1];
+      coordList.addCoord(seg.p0, false);
+      double len = seg.getLength();
+
+      // check if no densification is required
+      if (len <= distanceTolerance) continue;
+
+      // densify the segment
+      int densifiedSegCount = ((len / distanceTolerance)).ceil();
+      double densifiedSegLen = len / densifiedSegCount;
+      for (int j = 1; j < densifiedSegCount; j++) {
+        double segFract = (j * densifiedSegLen) / len;
+        Coordinate p = seg.pointAlong(segFract);
+        precModel.makeCoordinatePrecise(p);
+        coordList.addCoord(p, false);
+      }
+    }
+    // this check handles empty sequences
+    if (pts.length > 0) coordList.addCoord(pts[pts.length - 1], false);
+    return coordList.toCoordinateArray();
+  }
+
+ late Geometry inputGeom;
+
+  late double distanceTolerance;
+
+  /**
+   * Indicates whether areas should be topologically validated.
+   */
+  bool isValidated = true;
+
+  /**
+   * Creates a new densifier instance.
+   *
+   * @param inputGeom
+   */
+  Densifier(Geometry inputGeom) {
+    this.inputGeom = inputGeom;
+  }
+
+  /**
+   * Sets the distance tolerance for the densification. All line segments
+   * in the densified geometry will be no longer than the distance tolerance.
+   * The distance tolerance must be positive.
+   *
+   * @param distanceTolerance
+   *          the densification tolerance to use
+   */
+  void setDistanceTolerance(double distanceTolerance) {
+    if (distanceTolerance <= 0.0)
+      throw new Exception("Tolerance must be positive");
+    this.distanceTolerance = distanceTolerance;
+  }
+
+  /**
+   * Sets whether polygonal results are processed to ensure they are valid.
+   *
+   * @param isValidated true if the results should be validated
+   */
+  void setValidate(bool isValidated) {
+    this.isValidated = isValidated;
+  }
+
+  /**
+   * Gets the densified geometry.
+   *
+   * @return the densified geometry
+   */
+  Geometry getResultGeometry() {
+    return (new DensifyTransformer(distanceTolerance, isValidated))
+        .transform(inputGeom);
+  }
+}
+
+class DensifyTransformer extends GeometryTransformer {
+  late double distanceTolerance;
+  late bool isValidated;
+
+  DensifyTransformer(double distanceTolerance, bool isValidated) {
+    this.distanceTolerance = distanceTolerance;
+    this.isValidated = isValidated;
+  }
+
+  CoordinateSequence transformCoordinates(
+      CoordinateSequence coords, Geometry parent) {
+    List<Coordinate> inputPts = coords.toCoordinateArray();
+    List<Coordinate> newPts = Densifier.densifyPoints(
+        inputPts, distanceTolerance, parent.getPrecisionModel());
+    // prevent creation of invalid linestrings
+    if (parent is LineString && newPts.length == 1) {
+      newPts = [Coordinate.empty2D()];
+    }
+    return factory.getCoordinateSequenceFactory().create(newPts);
+  }
+
+   Geometry transformPolygon(Polygon geom, Geometry parent) {
+    Geometry roughGeom = super.transformPolygon(geom, parent);
+    // don't try and correct if the parent is going to do this
+    if (parent is MultiPolygon) {
+      return roughGeom;
+    }
+    return createValidArea(roughGeom);
+  }
+
+   Geometry transformMultiPolygon(MultiPolygon geom, Geometry parent) {
+    Geometry roughGeom = super.transformMultiPolygon(geom, parent);
+    return createValidArea(roughGeom);
+  }
+
+  /**
+   * Creates a valid area geometry from one that possibly has bad topology
+   * (i.e. self-intersections). Since buffer can handle invalid topology, but
+   * always returns valid geometry, constructing a 0-width buffer "corrects"
+   * the topology. Note this only works for area geometries, since buffer
+   * always returns areas. This also may return empty geometries, if the input
+   * has no actual area.
+   *
+   * @param roughAreaGeom
+   *          an area geometry possibly containing self-intersections
+   * @return a valid area geometry
+   */
+  Geometry createValidArea(Geometry roughAreaGeom) {
+    // if valid no need to process to make valid
+    if (!isValidated || roughAreaGeom.isValid()) return roughAreaGeom;
+    return roughAreaGeom.buffer(0.0);
+  }
+}
--- lib/src/com/hydrologis/dart_jts/geom/util.dart
+++ lib/src/com/hydrologis/dart_jts/geom/util.dart
@@ -1914,3 +1914,282 @@ abstract class CoordinateSequenceOperation implements GeometryEditorOperation {
    */
   CoordinateSequence editSeq(CoordinateSequence coordSeq, Geometry geometry);
 }
+
+
+/**
+ * A framework for processes which transform an input {@link Geometry} into
+ * an output {@link Geometry}, possibly changing its structure and type(s).
+ * This class is a framework for implementing subclasses
+ * which perform transformations on
+ * various different Geometry subclasses.
+ * It provides an easy way of applying specific transformations
+ * to given geometry types, while allowing unhandled types to be simply copied.
+ * Also, the framework ensures that if subcomponents change type
+ * the parent geometries types change appropriately to maintain valid structure.
+ * Subclasses will override whichever <code>transformX</code> methods
+ * they need to to handle particular Geometry types.
+ * <p>
+ * A typically usage would be a transformation class that transforms <tt>Polygons</tt> into
+ * <tt>Polygons</tt>, <tt>LineStrings</tt> or <tt>Points</tt>, depending on the geometry of the input
+ * (For instance, a simplification operation).
+ * This class would likely need to override the {@link #transformMultiPolygon(MultiPolygon, Geometry)}
+ * method to ensure that if input Polygons change type the result is a <tt>GeometryCollection</tt>,
+ * not a <tt>MultiPolygon</tt>.
+ * <p>
+ * The default behaviour of this class is simply to recursively transform
+ * each Geometry component into an identical object by deep copying down
+ * to the level of, but not including, coordinates.
+ * <p>
+ * All <code>transformX</code> methods may return <code>null</code>,
+ * to avoid creating empty or invalid geometry objects. This will be handled correctly
+ * by the transformer.   <code>transform<i>XXX</i></code> methods should always return valid
+ * geometry - if they cannot do this they should return <code>null</code>
+ * (for instance, it may not be possible for a transformLineString implementation
+ * to return at least two points - in this case, it should return <code>null</code>).
+ * The {@link #transform(Geometry)} method itself will always
+ * return a non-null Geometry object (but this may be empty).
+ *
+ * @version 1.7
+ *
+ * @see GeometryEditor
+ */
+class GeometryTransformer {
+  /**
+   * Possible extensions:
+   * getParent() method to return immediate parent e.g. of LinearRings in Polygons
+   */
+
+ late Geometry inputGeom;
+
+  late  GeometryFactory factory;
+
+  // these could eventually be exposed to clients
+  /**
+   * <code>true</code> if empty geometries should not be included in the result
+   */
+  bool pruneEmptyGeometry = true;
+
+  /**
+   * <code>true</code> if a homogenous collection result
+   * from a {@link GeometryCollection} should still
+   * be a general GeometryCollection
+   */
+  bool preserveGeometryCollectionType = true;
+
+  /**
+   * <code>true</code> if the output from a collection argument should still be a collection
+   */
+  bool preserveCollections = false;
+
+  /**
+   * <code>true</code> if the type of the input should be preserved
+   */
+  bool preserveType = false;
+
+  GeometryTransformer() {}
+
+  /**
+   * Utility function to make input geometry available
+   *
+   * @return the input geometry
+   */
+  Geometry getInputGeometry() {
+    return inputGeom;
+  }
+
+  Geometry transform(Geometry inputGeom) {
+    this.inputGeom = inputGeom;
+    this.factory = inputGeom.getFactory();
+
+    if (inputGeom is Point) return transformPoint(inputGeom as Point,inputGeom );
+    if (inputGeom is MultiPoint)
+      return transformMultiPoint(inputGeom as MultiPoint, inputGeom);
+    if (inputGeom is LinearRing)
+      return transformLinearRing(inputGeom as LinearRing, inputGeom);
+    if (inputGeom is LineString)
+      return transformLineString(inputGeom as LineString, inputGeom);
+    if (inputGeom is MultiLineString)
+      return transformMultiLineString(inputGeom as MultiLineString, inputGeom);
+    if (inputGeom is Polygon)
+      return transformPolygon(inputGeom as Polygon, inputGeom);
+    if (inputGeom is MultiPolygon)
+      return transformMultiPolygon(inputGeom as MultiPolygon, inputGeom);
+    if (inputGeom is GeometryCollection)
+      return transformGeometryCollection(inputGeom as GeometryCollection, inputGeom);
+
+    throw new Exception("Unknown Geometry subtype: ");
+  }
+
+  /**
+   * Convenience method which provides standard way of
+   * creating a {@link CoordinateSequence}
+   *
+   * @param coords the coordinate array to copy
+   * @return a coordinate sequence for the array
+   */
+  CoordinateSequence createCoordinateSequence(List<Coordinate> coords) {
+    return factory.getCoordinateSequenceFactory().create(coords);
+  }
+
+  /**
+   * Convenience method which provides a standard way of copying {@link CoordinateSequence}s
+   * @param seq the sequence to copy
+   * @return a deep copy of the sequence
+   */
+  CoordinateSequence copy(CoordinateSequence seq) {
+    return seq.copy();
+  }
+
+  /**
+   * Transforms a {@link CoordinateSequence}.
+   * This method should always return a valid coordinate list for
+   * the desired result type.  (E.g. a coordinate list for a LineString
+   * must have 0 or at least 2 points).
+   * If this is not possible, return an empty sequence -
+   * this will be pruned out.
+   *
+   * @param coords the coordinates to transform
+   * @param parent the parent geometry
+   * @return the transformed coordinates
+   */
+  CoordinateSequence transformCoordinates(
+      CoordinateSequence coords, Geometry parent) {
+    return copy(coords);
+  }
+
+  Geometry transformPoint(Point geom, Geometry parent) {
+    return factory.createPointSeq(
+        transformCoordinates(geom.getCoordinateSequence(), geom));
+  }
+
+  Geometry transformMultiPoint(MultiPoint geom, Geometry parent) {
+    List<Geometry> transGeomList = [];
+    for (int i = 0; i < geom.getNumGeometries(); i++) {
+      Geometry transformGeom =
+      transformPoint(geom.getGeometryN(i) as Point, geom);
+      if (transformGeom == null) continue;
+      if (transformGeom.isEmpty()) continue;
+      transGeomList.add(transformGeom);
+    }
+    if (transGeomList.isEmpty) {
+      return factory.createMultiPointEmpty();
+    }
+    return factory.buildGeometry(transGeomList);
+  }
+
+  /**
+   * Transforms a LinearRing.
+   * The transformation of a LinearRing may result in a coordinate sequence
+   * which does not form a structurally valid ring (i.e. a degenerate ring of 3 or fewer points).
+   * In this case a LineString is returned.
+   * Subclasses may wish to override this method and check for this situation
+   * (e.g. a subclass may choose to eliminate degenerate linear rings)
+   *
+   * @param geom the ring to simplify
+   * @param parent the parent geometry
+   * @return a LinearRing if the transformation resulted in a structurally valid ring
+   * @return a LineString if the transformation caused the LinearRing to collapse to 3 or fewer points
+   */
+  Geometry transformLinearRing(LinearRing geom, Geometry parent) {
+    CoordinateSequence seq =
+    transformCoordinates(geom.getCoordinateSequence(), geom);
+    if (seq == null) return factory.createLinearRingEmpty();
+    int seqSize = seq.size();
+    // ensure a valid LinearRing
+    if (seqSize > 0 && seqSize < 4 && !preserveType)
+      return factory.createLineStringSeq(seq);
+    return factory.createLinearRingSeq(seq);
+  }
+
+  /**
+   * Transforms a {@link LineString} geometry.
+   *
+   * @param geom
+   * @param parent
+   * @return
+   */
+  Geometry transformLineString(LineString geom, Geometry parent) {
+    // should check for 1-point sequences and downgrade them to points
+    return factory.createLineStringSeq(
+        transformCoordinates(geom.getCoordinateSequence(), geom));
+  }
+
+  Geometry transformMultiLineString(MultiLineString geom, Geometry parent) {
+    List<Geometry> transGeomList = [];
+    for (int i = 0; i < geom.getNumGeometries(); i++) {
+      Geometry transformGeom =
+      transformLineString(geom.getGeometryN(i) as LineString, geom);
+      if (transformGeom == null) continue;
+      if (transformGeom.isEmpty()) continue;
+      transGeomList.add(transformGeom);
+    }
+    if (transGeomList.isEmpty) {
+      return factory.createMultiLineStringEmpty();
+    }
+    return factory.buildGeometry(transGeomList);
+  }
+
+  Geometry transformPolygon(Polygon geom, Geometry parent) {
+    bool isAllValidLinearRings = true;
+    Geometry shell = transformLinearRing(geom.getExteriorRing(), geom);
+
+    // handle empty inputs, or inputs which are made empty
+    bool shellIsNullOrEmpty = shell.isEmpty();
+    if (geom.isEmpty() && shellIsNullOrEmpty) {
+      return factory.createPolygonEmpty();
+    }
+
+    if (shellIsNullOrEmpty || !(shell is LinearRing))
+      isAllValidLinearRings = false;
+
+    List<Geometry> holes = [];
+    for (int i = 0; i < geom.getNumInteriorRing(); i++) {
+      Geometry hole = transformLinearRing(geom.getInteriorRingN(i), geom);
+      if (hole == null || hole.isEmpty()) {
+        continue;
+      }
+      if (!(hole is LinearRing)) isAllValidLinearRings = false;
+
+      holes.add(hole);
+    }
+
+    if (isAllValidLinearRings)
+      return factory.createPolygon(shell as LinearRing, []);
+    else {
+      List<Geometry> components = [];
+      if (shell != null) components.add(shell);
+      components.addAll(holes);
+      return factory.buildGeometry(components);
+    }
+  }
+
+  Geometry transformMultiPolygon(MultiPolygon geom, Geometry parent) {
+    List<Geometry> transGeomList = [];
+    for (int i = 0; i < geom.getNumGeometries(); i++) {
+      Geometry transformGeom =
+      transformPolygon(geom.getGeometryN(i) as Polygon, geom);
+      if (transformGeom == null) continue;
+      if (transformGeom.isEmpty()) continue;
+      transGeomList.add(transformGeom);
+    }
+    if (transGeomList.isEmpty) {
+      return factory.createMultiPolygonEmpty();
+    }
+    return factory.buildGeometry(transGeomList);
+  }
+
+  Geometry transformGeometryCollection(
+      GeometryCollection geom, Geometry parent) {
+    List<Geometry> transGeomList = [];
+    for (int i = 0; i < geom.getNumGeometries(); i++) {
+      Geometry transformGeom = transform(geom.getGeometryN(i));
+      if (transformGeom == null) continue;
+      if (pruneEmptyGeometry && transformGeom.isEmpty()) continue;
+      transGeomList.add(transformGeom);
+    }
+    if (preserveGeometryCollectionType)
+      return factory.createGeometryCollection(transGeomList);
+    return factory.buildGeometry(transGeomList);
+  }
+}
+
